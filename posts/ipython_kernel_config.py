6cf0b83365980e5dab06bc118ec51dbe7069852c
Dan F-M <foreman.mackey@gmail.com>
2022-05-23 14:11:31 -0400

    trying to get tf post to work

--- posts/ipython_kernel_config.py
+++ posts/ipython_kernel_config.py
@@ -1,1 +1,8 @@
 c.InlineBackend.rc = {}
+c.InlineBackend.figure_format = "retina"
+
+from matplotlib import rcParams
+
+rcParams["savefig.dpi"] = 100
+rcParams["figure.dpi"] = 100
+rcParams["font.size"] = 20
